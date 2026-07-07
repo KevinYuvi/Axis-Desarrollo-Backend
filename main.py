e1849f4387ce21787ca2e7452cc2fa91fd6c5f17
prisc92 <pvchisag@uce.edu.ec>
2026-07-07 14:54:33 -0500

    crear modulo de tickets

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.reportes.router import router as reportes_router
 from app.usuarios.router import router as usuarios_router
 from app.incidencias.router import router as incidencias_router
 from app.tickets.router import router as tickets_router
+from app.aulas.router import router as aulas_router
 
 
 app = FastAPI(
@@ -19,6 +20,7 @@ app.include_router(reportes_router)
 app.include_router(usuarios_router)
 app.include_router(incidencias_router)
 app.include_router(tickets_router)
+app.include_router(aulas_router)
 
 
 @app.get("/", tags=["Root"])
@@ -28,7 +30,4 @@ def read_root():
         "message": "Bienvenido a la API de AXIS Backend",
     }
 
-app.include_router(espacios_router)
-app.include_router(reservas_router)
-app.include_router(reportes_router)
-app.include_router(usuarios_router)
+
